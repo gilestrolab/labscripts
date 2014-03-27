14c649842bceabe4ee862803026877f096f9c9df
Quentin Geissmann <qgeissmann@gmail.com>
2014-03-27 15:19:12 +0000

    Update post_arch_install.sh

--- post_arch_install.sh
+++ post_arch_install.sh
@@ -22,11 +22,18 @@ yaourt -S yaourt
 # very useful:
 yaourt -S bash-completion
 
+### Video capture
 # what we need for the lab
 yaourt -S opencv libdc1394-git mplayer ffmpeg gstreamer gstreamer0.10-plugins 
-
 # it is important that coriender is installed AFTER libdc1394-git
 yaourt -S coriander
-
 # adding udev rule for our camera
 sudo sh -c 'echo SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"1e10\", ATTRS{idProduct}==\"3006\", GROUP=\"plugdev\", SYMLINK+=\"firefly\", MODE:=\"0666\" > /etc/udev/rules.d/30-my.rules'
+
+
+# we would like scientific packages:
+yaourt -S imagej r octave python{2,}-scikit-learn  python{2,}-scipy imagemagick
+
+# setting up ssh server
+sudo systemctl enable ssh.service 
+sudo systemctl start ssh.service
