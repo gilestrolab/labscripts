a4b42f025992c27eb5f72504ea39666d071aa393
Quentin Geissmann <qgeissmann@gmail.com>
2014-08-27 17:19:54 +0100

    add latex hack. simple csv table for @anne

--- rsync_backup.sh
+++ rsync_backup.sh
@@ -7,6 +7,6 @@ HOST_NAME=155.198.235.200
 USER=$(whoami)
 REMOTE_DIR=/data/sleepysnail/raw/
 
-rsync -e ssh -aru $HOST_NAME:$REMOTE_DIR $REMOTE_DIR
+rsync -e ssh -aruv $HOST_NAME:$REMOTE_DIR $REMOTE_DIR
 
 # $REMOTE_DIR MUST exixt on your the local machine (and have the appropriate permission)
