d1b47b10071811ac667e9ab6f2117f88aad7eb83
Quentin Geissmann <qgeissmann@gmail.com>
2014-10-23 14:41:45 +0100

    add arduino ide installation + use packer instead of yaourt

--- post_arch_install.sh
+++ post_arch_install.sh
@@ -1,47 +1,37 @@
 ### to setup a computer in the lab just after installing arch:
 
-sudo pacman -S base-devel
+yaourt -S  packer --noconfirm
 
-cd /tmp/
-curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
-tar zxvf package-query.tar.gz
-cd package-query
-makepkg -si
-sudo pacman -U *.pkg.tar.xz  
-cd ..
-curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
-tar zxvf yaourt.tar.gz
-cd yaourt
-makepkg -si
-sudo pacman -U *.pkg.tar.xz
-cd ..
-
-#bootstrapping yaourt
-yaourt -S yaourt
-
-# very useful:
-yaourt -S bash-completion
+packer -S bash-completion base-devel  --noconfirm
 
 ### Video capture
 # what we need for the lab
-yaourt -S opencv libdc1394-git mplayer ffmpeg gstreamer gstreamer0.10-plugins mencoder
+packer -S opencv libdc1394-git mplayer ffmpeg gstreamer gstreamer0.10-plugins mencoder   --noconfirm 
 # it is important that coriender is installed AFTER libdc1394-git
-yaourt -S coriander
+packer -S coriander  --noconfirm
 # adding udev rule for our camera
 sudo sh -c 'echo SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"1e10\", ATTRS{idProduct}==\"3006\", GROUP=\"plugdev\", SYMLINK+=\"firefly\", MODE:=\"0666\" > /etc/udev/rules.d/30-my.rules'
 
 
 # we would like scientific packages:
-yaourt -S imagej r octave python{2,}-scikit-learn  python{2,}-scipy imagemagick
+packer -S imagej r octave python{2,}-scikit-learn python{2,}-numpy  python{2,}-scipy imagemagick   --noconfirm
 
 
 # Python's pips
-yaourt -S python-pip python2-pip
+packer -S python-pip python2-pip ipython{2,}  --noconfirm 
 
 # system tools
-yaourt -S htop tree rsync
+packer -S htop tree rsync   --noconfirm 
 
 
 # setting up ssh server
 sudo systemctl enable ssh.service 
 sudo systemctl start ssh.service
+
+# arduino
+
+sudo gpasswd -a $USER uucp
+sudo gpasswd -a $USER lock
+sudo gpasswd -a $USER tty
+
+packer -S arduide-git --noconfirm
