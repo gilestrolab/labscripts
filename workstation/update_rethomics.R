13cabf717e3e18034affe73272645ff1397061e1
Quentin Geissmann <qgeissmann@gmail.com>
2015-09-10 12:46:21 +0100

    Update update_rethomics.R

--- workstation/update_rethomics.R
+++ workstation/update_rethomics.R
@@ -1,3 +1,4 @@
 # this is a simple script to keep rethomics up to date on the workstations. Typically run on a cronjob.
+# save this file at `/root/update_rethomics.R`
 library(devtools)
 devtools::install_github("gilestrolab/rethomics",subdir="rethomics")
