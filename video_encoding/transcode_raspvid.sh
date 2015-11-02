8c0000582150b3844e4bbf7b6bf69ab70bd20aab
Quentin Geissmann <qgeissmann@gmail.com>
2015-11-02 15:07:36 +0000

    Update transcode_raspvid.sh

--- video_encoding/transcode_raspvid.sh
+++ video_encoding/transcode_raspvid.sh
@@ -1,2 +1,2 @@
-
-ffmpeg -r 25 -i video.h264 -vcodec copy video.mp4
+# `-r 20` means you have use a fps of 20 when acquiring the video (that is `-fps 20` in raspivid)
+ffmpeg -r 20 -i video.h264 -vcodec copy video.mp4
