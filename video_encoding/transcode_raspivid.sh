7e12162e4c85cda306d8f6420dba83a1f9f3b309
Quentin Geissmann <qgeissmann@gmail.com>
2015-11-02 15:13:54 +0000

    Update and rename transcode_raspvid.sh to transcode_raspivid.sh

--- video_encoding/transcode_raspivid.sh
+++ video_encoding/transcode_raspivid.sh
@@ -1,2 +1,4 @@
 # `-r 20` means you have use a fps of 20 when acquiring the video (that is `-fps 20` in raspivid)
 ffmpeg -r 20 -i video.h264 -vcodec copy video.mp4
+# to compress video on the go, one can do:
+ffmpeg -r 20 -i input.h264  -c:v libx264 -preset medium -tune ssim -crf 28 output.mp4
