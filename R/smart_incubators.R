e0a49d2d9c429d99dc2b8f0e1977748f72f54b5f
Quentin Geissmann <qgeissmann@gmail.com>
2016-02-11 10:00:01 +0000

    Update smart_incubators.R

--- R/smart_incubators.R
+++ R/smart_incubators.R
@@ -3,26 +3,31 @@ rm(list = ls(all = TRUE))
 library(data.table)
 library(ggplot2)
 
+URL <- "ftp://etho-node.lab.gilest.ro/ardufly-data/smart_incubator.csv"
+
 #' Unix time stammp to UTC date
 asDateTime <- function(unixts){
   as.POSIXct(unixts, origin="1970-01-01",tz = "UTC")
 }
 
-URL <- "ftp://etho-node.lab.gilest.ro/ardufly-data/smart_incubator.csv"
+readIncubatorFile <- function(file){
+  # directly load data from ftp!
+  dt <- fread(file)
+  #change unix time stamp to date objects
+  dt[, device_time:=asDateTime(device_time)]
+  dt[, server_time:=asDateTime(server_time)]
+  dt[, t:=device_time]
+  dt
+}
 
-# directly load data from ftp!
-dt <- fread(URL)
 
-change unix time stamp to date objects
-dt[, device_time:=asDateTime(device_time)]
-dt[, server_time:=asDateTime(server_time)]
-
+dt <- readIncubatorFile(URL)
 # just a few exemples, showing one variable vs time one facet per incubator
 # show temperature
-ggplot(dt,aes(server_time, temperature)) + geom_line() + geom_point() + facet_grid(id ~ .)
+ggplot(dt,aes(t, temperature)) + geom_line() + geom_point() + facet_grid(id ~ .)
 
 #show_light
-ggplot(dt,aes(server_time, light)) + geom_line() + geom_point() + facet_grid(id ~ .)
+ggplot(dt,aes(t, light)) + geom_line() + geom_point() + facet_grid(id ~ .)
 
 
 
