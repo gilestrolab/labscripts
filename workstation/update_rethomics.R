ad699a0ff07ca0460b627cd8b82f8563ecc295be
Quentin Geissmann <qgeissmann@gmail.com>
2016-02-17 18:29:47 +0000

    fix rsync workstation

--- workstation/update_rethomics.R
+++ workstation/update_rethomics.R
@@ -1,5 +1,5 @@
 # this is a simple script to keep rethomics up to date on the workstations. Typically run on a cronjob.
 # save this file at `/root/update_rethomics.R`
-install.packages("devtools")
+install.packages("devtools", repos='http://cran.uk.r-project.org')
 library(devtools)
 devtools::install_github("gilestrolab/rethomics",subdir="rethomics")
