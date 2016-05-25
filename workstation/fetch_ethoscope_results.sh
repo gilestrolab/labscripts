5a0630a4dca4f8f2666282b1b3ac7962fe2a1955
Quentin Geissmann <qgeissmann@gmail.com>
2016-05-25 15:14:21 +0100

    Update fetch_ethoscope_results.sh

--- workstation/fetch_ethoscope_results.sh
+++ workstation/fetch_ethoscope_results.sh
@@ -1,4 +1,16 @@
-DAVFS_MNT_DIR="/mnt/box.com"
-mount $DAVFS_MNT_DIR
-rsync -avhP --size-only  --inplace  $DAVFS_MNT_DIR/gglab/shared/ethoscope_results/ /data/ethoscope_results/ --exclude=lost+found --exclude="*.db-journal" --exclude="*.zip" --exclude="*.db.*"
-umount $DAVFS_MNT_DIR
+#!/bin/bash
+
+result_dir=ethoscope_results/
+source_prefix=/mnt/dav/auto_generated_data/
+target_prefix=/data/
+(
+  ## we use flock to ensure npo concurent backups
+  # see http://stackoverflow.com/questions/169964/how-to-prevent-a-script-from-running-simultaneously
+  # Wait for lock on /var/lock/.myscript.exclusivelock (fd 200) for 10 seconds
+  flock -x -w 10 200 || exit 1
+  systemctl stop $backup_service && 
+  sleep 30 &&
+  rsync -avhP --size-only  --inplace  $source_prefix/$result_dir/  $target_prefix/$result_dir/  --exclude=lost+found --exclude="*.db-journal" --exclude="*.zip" --exclude="*.db.*"
+  systemctl start $backup_service
+
+) 200>/var/lock/.myscript.exclusivelock
