635341eeb63ce4af5ac194945ea43d9dfbf69150
Quentin Geissmann <qgeissmann@gmail.com>
2016-05-25 14:46:00 +0100

    Update push_ethoscope_results.sh

--- node/push_ethoscope_results.sh
+++ node/push_ethoscope_results.sh
@@ -1,4 +1,5 @@
-DAVFS_MNT_DIR="/mnt/box.com"
-mount $DAVFS_MNT_DIR
-rsync -avhP --size-only  --inplace  /ethoscope_results/  $DAVFS_MNT_DIR/gglab/shared/ethoscope_results/  --exclude=lost+found --exclude="*.db-journal" --exclude="*.zip" --exclude="*.db.*"
-umount $DAVFS_MNT_DIR
+systemctl stop ethoscope_backup && 
+sleep 30 &&
+rsync -avhP --size-only  --inplace  /ethoscope_results/  /mnt/dav/auto_generated_data/ethoscope_results/  --exclude=lost+found --exclude="*.db-journal" --exclude="*.zip" --exclude="*.db.*"
+systemctl start ethoscope_backup
+
