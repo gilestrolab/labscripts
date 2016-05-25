fbfe902a3d5ed0287565c975971ec2c81c5f9fa4
Quentin Geissmann <qgeissmann@gmail.com>
2016-05-25 14:57:42 +0100

    Update push_ethoscope_videos.sh

--- node/push_ethoscope_videos.sh
+++ node/push_ethoscope_videos.sh
@@ -1,4 +1,19 @@
-systemctl stop ethoscope_video_backup && 
-sleep 30 &&
-rsync -avhP --size-only  --inplace  /ethoscope_videos/  /mnt/dav/auto_generated_data/ethoscope_videos/  --exclude=lost+found
-systemctl start ethoscope_video backup
+#!/bin/bash
+# we ensure the backup service is stopped durring backup, so we do not keep modifiying files
+# during backup 
+backup_service=ethoscope_video_backup
+result_dir=ethoscope_videos/
+target_prefix=/mnt/dav/auto_generated_data/
+(
+  ## we use flock to ensure npo concurent backups
+  # see http://stackoverflow.com/questions/169964/how-to-prevent-a-script-from-running-simultaneously
+  # Wait for lock on /var/lock/.myscript.exclusivelock (fd 200) for 10 seconds
+  flock -x -w 10 200 || exit 1
+  systemctl stop $backup_service && 
+  sleep 30 &&
+  rsync -avhP --size-only  --inplace  /$result_dir/  $target_prefix/$result_dir/  --exclude=lost+found --exclude="*.db-journal" --exclude="*.zip" --exclude="*.db.*"
+  systemctl start $backup_service
+
+) 200>/var/lock/.myscript.exclusivelock
+
+
