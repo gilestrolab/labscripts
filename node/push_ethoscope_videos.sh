24ff4c0d29785455ae95800ae025b1ab99554597
Quentin Geissmann <qgeissmann@gmail.com>
2016-05-27 14:38:56 +0100

    Update push_ethoscope_videos.sh

--- node/push_ethoscope_videos.sh
+++ node/push_ethoscope_videos.sh
@@ -3,7 +3,7 @@
 # during backup 
 backup_service=ethoscope_video_backup
 result_dir=ethoscope_videos/
-target_prefix=/mnt/dav/auto_generated_data/
+target_prefix=/mnt/nas/auto_generated_data/
 (
   ## we use flock to ensure npo concurent backups
   # see http://stackoverflow.com/questions/169964/how-to-prevent-a-script-from-running-simultaneously
@@ -14,6 +14,6 @@ target_prefix=/mnt/dav/auto_generated_data/
   rsync -avhP --size-only  --inplace  /$result_dir/  $target_prefix/$result_dir/  --exclude=lost+found --exclude="*.db-journal" --exclude="*.zip" --exclude="*.db.*"
   systemctl start $backup_service
 
-) 200>/var/lock/.myscript.exclusivelock
+) 200>/var/lock/.$backup_service.exclusivelock
 
 
