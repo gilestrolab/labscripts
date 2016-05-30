617fabb4f3c9e20fa5676d823fde0ce6db713d98
Quentin Geissmann <qgeissmann@gmail.com>
2016-05-30 10:49:48 +0100

    Update fetch_ethoscope_results.sh

--- workstation/fetch_ethoscope_results.sh
+++ workstation/fetch_ethoscope_results.sh
@@ -8,9 +8,5 @@ target_prefix=/data/
   # see http://stackoverflow.com/questions/169964/how-to-prevent-a-script-from-running-simultaneously
   # Wait for lock on /var/lock/.myscript.exclusivelock (fd 200) for 10 seconds
   flock -x -w 10 200 || exit 1
-  systemctl stop $backup_service && 
-  sleep 30 &&
   rsync -avhP --size-only  --inplace  $source_prefix/$result_dir/  $target_prefix/$result_dir/  --exclude=lost+found --exclude="*.db-journal" --exclude="*.zip" --exclude="*.db.*"
-  systemctl start $backup_service
-
-) 200>/var/lock/.myscript.exclusivelock
+) 200>/var/lock/.$(basename $result_dir).exclusivelock
