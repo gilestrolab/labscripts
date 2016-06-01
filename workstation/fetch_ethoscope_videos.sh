8dd8197c5886709a6171f08ba8676f6c0de25f33
Quentin Geissmann <qgeissmann@gmail.com>
2016-06-01 17:17:16 +0100

    Update fetch_ethoscope_videos.sh

--- workstation/fetch_ethoscope_videos.sh
+++ workstation/fetch_ethoscope_videos.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-
 result_dir=ethoscope_videos/
 source_prefix=/mnt/nas/auto_generated_data/
 target_prefix=/data/
 (
-  ## we use flock to ensure npo concurent backups
-  # see http://stackoverflow.com/questions/169964/how-to-prevent-a-script-from-running-simultaneously
-  # Wait for lock on /var/lock/.myscript.exclusivelock (fd 200) for 10 seconds
   flock -x -w 10 200 || exit 1
-  systemctl stop $backup_service && 
-  sleep 30 &&
-  rsync -avhP --size-only  --inplace  $source_prefix/$result_dir/  $target_prefix/$result_dir/  --exclude=lost+found --exclude="*.db-journal" --exclude="*.zip" --exclude="*.db.*"
+  rsync -avhP --size-only  --inplace  $source_prefix/$result_dir/  $target_prefix/$result_dir/  --exclude=lost+found
   systemctl start $backup_service
-
-) 200>/var/lock/.myscript.exclusivelock
+) 200>/var/lock/.$(basename $result_dir).exclusivelock
