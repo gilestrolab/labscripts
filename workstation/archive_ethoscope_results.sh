7b8cfe33705358580963c13360f52ba4c9a05ec7
Quentin Geissmann <qgeissmann@gmail.com>
2016-06-03 16:20:59 +0100

    Update archive_ethoscope_results.sh

--- workstation/archive_ethoscope_results.sh
+++ workstation/archive_ethoscope_results.sh
@@ -5,10 +5,12 @@
 src=/data/ethoscope_results/
 dst_prefix=/mnt/box.com
 dst_dir=/shared/ethoscope_results
+log_file=/tmp/archive_result_log.txt
 (
   flock -x -w 10 200 || exit 1
   umount dst_prefix
   mount dst_prefix &&
-  rsync -ahW --stats --size-only  --inplace  $src  $dst_prefix/$dst_dir/
+  echo "Starting backup" &&
+  rsync -ahWPv --stats --size-only  --inplace  $src  $dst_prefix/$dst_dir/ > $log_file
   umount dst_prefix
 ) 200>/var/lock/.$(basename $src).exclusivelock
