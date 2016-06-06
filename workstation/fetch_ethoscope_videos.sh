5d8b9d1ab0b8d3cb60f21da6de19521d8fc0b306
Quentin Geissmann <qgeissmann@gmail.com>
2016-06-06 14:15:18 +0100

    Update fetch_ethoscope_videos.sh

--- workstation/fetch_ethoscope_videos.sh
+++ workstation/fetch_ethoscope_videos.sh
@@ -5,5 +5,4 @@ target_prefix=/data/
 (
   flock -x -w 10 200 || exit 1
   rsync -avhP --size-only  --inplace  $source_prefix/$result_dir/  $target_prefix/$result_dir/  --exclude=lost+found
-  systemctl start $backup_service
 ) 200>/var/lock/.$(basename $result_dir).exclusivelock
