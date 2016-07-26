e7f64bd02266cc9337cd6daac380e8cf284e400e
Quentin Geissmann <qgeissmann@gmail.com>
2016-07-26 14:55:37 +0100

    Update ggplot_themes.R

--- R/ggplot_themes.R
+++ R/ggplot_themes.R
@@ -6,9 +6,26 @@
 # # change theme and show:
 # `pl + ethogram_theme`
 
-ethogram_theme <- theme_bw() + theme(	axis.text = element_text(size = 12),
+# This is designed to for pdf of size 8x6 inches. 
+# So, unless you have a good reason to use a different size, you can do:
+# `pdf("my_file_name.pdf", w=8,h=6)`
+# `do stuf...`
+# `dev.off`
+
+ethogram_theme <- theme_bw() + theme(	 axis.text = element_text(size = 12),
 					 panel.grid.major = element_line(colour = "grey80"),
 					 panel.grid.major.x =element_blank(),
 					 panel.grid.minor = element_blank(),
 					 legend.position = "bottom",
 					 legend.title =element_blank())
+
+
+# this theme is for generic scatterplots, boxplot, barplot...
+generic_theme <- theme_bw()+ theme( 	axis.text = element_text(size = 12),
+					axis.title=element_text(size=14),
+					axis.title=element_text(size=14),
+					axis.text.x = element_text(angle=60, vjust=1.1, hjust=1),
+					panel.grid.major = element_line(colour = "grey80"),
+					panel.grid.major.x =element_blank(),
+					panel.grid.minor = element_blank(),
+					legend.position = "none")
