f7746d3eb4ac733ed6663772398ac5fc940c39bd
nazabic <diana.bicazan@gmail.com>
2016-10-21 19:12:14 +0100

    select data since date in sql before fetching to data.table

--- R/fetch_inc_dt.R
+++ R/fetch_inc_dt.R
@@ -5,26 +5,28 @@ library(ggplot2)
 library(RMySQL)
 
 mydb = dbConnect(MySQL(), user='incubators', password='incubators', dbname='incubators', host='etho-node.lab.gilest.ro')
-rs = dbSendQuery(mydb, "select * from incubators")
-rs
-data = fetch(rs, n=-1)
-dt <- data.table(data)
-
-
 
 asDateTime <- function(unixts){
   as.POSIXct(unixts, origin="1970-01-01",tz = "UTC")
 }
 
-
-fetchIncubatorData <- function(dt, since="1970-01-01"){
+fetchIncubatorData <- function(db, since="1970-01-01 00:00:00", to=NULL){
+  if(is.null(to)){
+    to <- as.character(Sys.time())
+  }
+  
+  since <- paste("'", since, "'")
+  to <- paste("'", to, "'")
+  query <- paste("SELECT * FROM incubators WHERE device_time  BETWEEN", since, "AND", to)
+  rs =dbSendQuery(db, query)
+  data = fetch(rs, n=-1)
+  dt <- data.table(data)
   dt[, device_time:=asDateTime(device_time)]
   dt[, t:=device_time]
-  dt[t >= since]
 }
 
 
-my_dt <- fetchIncubatorData(dt, since = "2016-07-30")
+my_dt <- fetchIncubatorData(mydb, since = "2016-07-29")
 
 ggplot(my_dt,aes(t, temperature)) + geom_line() + geom_point() + facet_grid(id ~ .)
 
