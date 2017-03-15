299a599c5ddc47850169e919984b1c001b89c705
Quentin Geissmann <qgeissmann@gmail.com>
2017-03-15 16:33:15 +0000

    Update make_db_index.R

--- node/make_db_index.R
+++ node/make_db_index.R
@@ -5,4 +5,4 @@ all_db_files <- list.files(RESULT_DIR,recursive=T, pattern="*\\.db$")
 fields <- strsplit(all_db_files,"/")
 valid_files <- sapply(fields,length) == 4
 all_db_files <- all_db_files[valid_files]
-write(all_db_files, paste(RESULT_DIR, INDEX,sep="/")
+write(all_db_files, paste(RESULT_DIR, INDEX,sep="/"))
